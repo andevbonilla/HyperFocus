a0436af82c6f03fadcce0223afbe75077b407715
Andres Bonilla <andresbonilla1256@gmail.com>
2025-08-02 16:42:49 -0400

    delete unused js file

--- scripts/blocked.js
+++
@@ -1,8 +0,0 @@
-// blocked.js
-(() => {
-    const params = new URLSearchParams(location.search);
-    const host = params.get('host') || '';
-    const el = document.getElementById('host');
-    if (el) el.textContent = host.replace(/^https?:\/\//,'');
-})();
-  
